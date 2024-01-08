9810b3dbfcf4444935394dd74283fe58b1203c45
Salma-Hamzaoui <hamzaoui.salma@gmail.com>
2024-01-08 14:18:28 -0800

    cool

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: the entered number
+ * Return: the value of the last digit
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
