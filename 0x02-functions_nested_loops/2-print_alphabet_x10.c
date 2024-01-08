252c3e327bc21317cb48d8fce9bfeb174c0984e1
Salma-Hamzaoui <hamzaoui.salma@gmail.com>
2024-01-08 12:23:40 -0800

    no test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function
+ * Description: function that write the alphabet 10 times
+ * Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123 ; i++)
+		_putchar(i);
+	}
+	_putchar('\n');
+}
