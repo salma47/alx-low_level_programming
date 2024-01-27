7efe15c307ba83d1fe1bdf126717e09249600415
Salma-Hamzaoui <hamzaoui.salma@gmail.com>
2024-01-27 14:32:29 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * swap_int - swap two integers
+ *
+ * @a: first integer
+ * @b: second intzger
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *j;
+
+	j = *b;
+	*b = *a;
+	*a = *j;
+}
